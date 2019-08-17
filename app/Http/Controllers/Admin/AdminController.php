674becd241e59a8a0c695f0551f837beb1c20b59
MrMicky <git@mrmicky.fr>
2019-08-17 14:32:16 +0200

    Add recent users chart to admin dashboard

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Page;
 use App\Models\Post;
 use App\Models\User;
+use Carbon\Carbon;
 
 class AdminController extends Controller
 {
@@ -14,7 +15,33 @@ class AdminController extends Controller
         return view('admin.dashboard')->with([
             'userCount' => User::count(),
             'postCount' => Post::count(),
-            'pageCount' => Page::count()
+            'pageCount' => Page::count(),
+            'recentUsers' => $this->getRecentUsers()
         ]);
+    }
+
+    protected function getRecentUsers()
+    {
+        $carbon = Carbon::now();
+        $month = $carbon->month;
+        $recentUsers = [];
+
+        $queryUsers = User::query()
+            ->whereDate('created_at', '>=', $carbon->subMonths(6))
+            ->get(['id', 'created_at'])
+            ->groupBy(function (User $user) {
+                return $user->created_at->format('Y-m');
+            })->transform(function ($data) {
+                return count($data);
+            });
+
+        while ($carbon->month < $month) {
+            $carbon->addMonth();
+            $time = $carbon->format('Y-m');
+
+            $recentUsers[$time] = $queryUsers->get($time, 0);
+        }
+
+        return $recentUsers;
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,6 +2,10 @@
 
 @section('title', 'Dashboard')
 
+@push('scripts')
+    <script src="{{asset('assets/admin/vendor/chart.js/Chart.min.js')}}"></script>
+@endpush
+
 @section('content')
     <!-- Content Row -->
     <div class="row">
@@ -57,4 +61,97 @@
             </div>
         </div>
     </div>
+
+    <div class="row">
+        <!-- Area Chart -->
+        <div class="col-xl-8 col-lg-7">
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Recent users</h6>
+                </div>
+                <div class="card-body">
+                    <div class="chart-area">
+                        <canvas id="usersChart"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
+
+@push('footer-scripts')
+    <script>
+        var recentUsersKeys = (@json(array_keys($recentUsers)));
+        var recentUsersValues = (@json(array_values($recentUsers)));
+
+        new Chart(document.getElementById("usersChart"), {
+            type: 'line',
+            data: {
+                labels: recentUsersKeys,
+                datasets: [{
+                    label: "New users",
+                    lineTension: 0.3,
+                    backgroundColor: "rgba(78, 115, 223, 0.05)",
+                    borderColor: "rgba(78, 115, 223, 1)",
+                    pointRadius: 3,
+                    pointBackgroundColor: "rgba(78, 115, 223, 1)",
+                    pointBorderColor: "rgba(78, 115, 223, 1)",
+                    pointHoverRadius: 3,
+                    pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+                    pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+                    pointHitRadius: 10,
+                    pointBorderWidth: 2,
+                    data: recentUsersValues,
+                }],
+            },
+            options: {
+                maintainAspectRatio: false,
+                scales: {
+                    xAxes: [{
+                        /*time: {
+                            unit: 'date'
+                        },*/
+                        gridLines: {
+                            display: false,
+                            drawBorder: false
+                        },
+                        /*ticks: {
+                            maxTicksLimit: 7
+                        }*/
+                    }],
+                    yAxes: [{
+                        /*ticks: {
+                            maxTicksLimit: 5,
+                            padding: 10,
+                        },*/
+                        gridLines: {
+                            color: "rgb(234, 236, 244)",
+                            zeroLineColor: "rgb(234, 236, 244)",
+                            drawBorder: false,
+                            borderDash: [2],
+                            zeroLineBorderDash: [2]
+                        }
+                    }],
+                },
+                legend: {
+                    display: false
+                },
+                tooltips: {
+                    backgroundColor: "rgb(255,255,255)",
+                    bodyFontColor: "#858796",
+                    titleMarginBottom: 10,
+                    titleFontColor: '#6e707e',
+                    titleFontSize: 14,
+                    borderColor: '#dddfeb',
+                    borderWidth: 1,
+                    xPadding: 15,
+                    yPadding: 15,
+                    displayColors: false,
+                    intersect: false,
+                    mode: 'index',
+                    caretPadding: 10,
+                }
+            }
+        });
+    </script>
+@endpush
