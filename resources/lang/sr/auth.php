ecc6526d7d329ef1f3da39789b9a0573aee4e776
MrMicky <git@mrmicky.fr>
2020-02-01 15:56:46 +0100

    New translations auth.php (Serbian (Cyrillic))

---
+++ resources/lang/sr/auth.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+    'register' => 'Register',
+    'login' => 'Login',
+    'logout' => 'Logout',
+    'verify' => 'Verify Your Email Address',
+    'passwords' => [
+        'confirm' => 'Confirm password',
+        'reset' => 'Reset Password',
+        'send' => 'Send Password Reset Link',
+    ],
+
+    'name' => 'Username',
+    'email' => 'E-Mail Address',
+    'password' => 'Password',
+    'confirm-password' => 'Confirm Password',
+    'current-password' => 'Current password',
+
+    'conditions' => 'I accept the <a href=":conditions" target="_blank">conditions</a>.',
+
+    '2fa-code' => 'Two Factor Auth Code',
+    '2fa-invalid' => 'Invalid code',
+
+    'maintenance' => 'The website is under maintenance.',
+
+    'remember-me' => 'Remember Me',
+    'forgot-password' => 'Forgot Your Password?',
+
+    'verify-sent' => 'A fresh verification link has been sent to your email address.',
+    'verify-check' => 'Before proceeding, please check your email for a verification link.',
+    'verify-request' => 'If you did not receive the email you can request another.',
+    'verify-resend' => 'Resend mail',
+
+    'need-confirm' => 'Please confirm your password before continuing.',
+
+];
