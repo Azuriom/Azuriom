7a93e4212c132d658b47da7eb1f2223d52ec0a1c
MrMicky <git@mrmicky.fr>
2020-02-01 15:56:16 +0100

    New translations errors.php (Ukrainian)

---
+++ resources/lang/uk/errors.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    'error' => 'Error',
+    'home' => 'Back to home',
+    'whoops' => 'Whoops !',
+
+    '401' => [
+        'title' => 'Unauthorized',
+        'message' => 'Unauthorized',
+    ],
+    '403' => [
+        'title' => 'Forbidden',
+        'message' => 'Forbidden',
+    ],
+    '404' => [
+        'title' => 'Not Found',
+        'message' => 'Not Found',
+    ],
+    '419' => [
+        'title' => 'Page Expired',
+        'message' => 'Page Expired',
+    ],
+    '429' => [
+        'title' => 'Too Many Requests',
+        'message' => 'Too Many Requests',
+    ],
+    '500' => [
+        'title' => 'Server Error',
+        'message' => 'Server Error',
+    ],
+    '503' => [
+        'title' => 'Service Unavailable',
+        'message' => 'Service Unavailable',
+    ],
+
+    'fallback' => [
+        'message' => 'An error occurred. HTTP code: :code',
+    ],
+];
