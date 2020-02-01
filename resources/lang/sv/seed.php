c370adde8a546ee0b4439f54163110bed53b84ec
MrMicky <git@mrmicky.fr>
2020-02-01 15:56:03 +0100

    New translations seed.php (Swedish)

---
+++ resources/lang/sv/seed.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Seed Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for seeding during installation.
+    |
+    */
+
+    'navbar' => [
+        'home' => 'Home',
+    ],
+
+    'roles' => [
+        'member' => 'Member',
+        'admin' => 'Admin',
+    ],
+
+    'settings' => [
+        'copyright' => 'Copyright © 2019 Azuriom - All Rights Reserved.',
+        'money' => 'points',
+    ],
+];
