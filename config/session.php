1324019bd3d53d9f2bbb2a0ee94ecb51bafd977f
MrMicky <git@mrmicky.fr>
2020-02-07 18:22:39 +0100

    Update config

--- config/session.php
+++ config/session.php
@@ -124,10 +124,7 @@ return [
     |
     */
 
-    'cookie' => env(
-        'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
-    ),
+    'cookie' => 'azuriom_session',
 
     /*
     |--------------------------------------------------------------------------
