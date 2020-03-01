a4172e7b81b71c8ed732881bd0364ac1d7d38b3b
MrMicky <git@mrmicky.fr>
2020-03-01 19:10:48 +0100

    Fix plugins views for themes

--- app/View/ThemeViewFinder.php
+++ app/View/ThemeViewFinder.php
@@ -3,6 +3,7 @@
 namespace Azuriom\View;
 
 use Illuminate\View\FileViewFinder;
+use InvalidArgumentException;
 
 class ThemeViewFinder extends FileViewFinder
 {
@@ -11,7 +12,11 @@ class ThemeViewFinder extends FileViewFinder
         [$namespace, $view] = $this->parseNamespaceSegments($name);
 
         if (plugins()->isEnabled($namespace)) {
-            return $this->find("plugins.{$namespace}.{$view}");
+            try {
+                return $this->find("plugins.{$namespace}.{$view}");
+            } catch (InvalidArgumentException $e) {
+                // ignore, theme don't have the plugin view, just use the default view
+            }
         }
 
         return $this->findInPaths($view, $this->hints[$namespace]);
