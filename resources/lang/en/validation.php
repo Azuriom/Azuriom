d3ae8ea3421c7c0bb114492780ea99a7f2c37062
MrMicky <git@mrmicky.fr>
2020-03-01 19:17:28 +0100

    Update validation translations

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -114,7 +114,7 @@ return [
     'string' => 'The :attribute must be a string.',
     'timezone' => 'The :attribute must be a valid zone.',
     'unique' => 'The :attribute has already been taken.',
-    'uploaded' => 'The :attribute failed to upload.',
+    'uploaded' => 'The :attribute failed to upload. Maybe this one\'s too heavy.',
     'url' => 'The :attribute format is invalid.',
     'uuid' => 'The :attribute must be a valid UUID.',
 
