0f8d1ec1351e797ba2b00d025ad0e3e0ae8c60e8
MrMicky <git@mrmicky.fr>
2020-05-07 17:39:26 +0200

    Add users search

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -3,6 +3,22 @@
 @section('title', trans('admin.users.title'))
 
 @section('content')
+    <form class="form-inline mb-3" action="{{ route('admin.users.index') }}" method="GET">
+        <div class="form-group mb-2">
+            <label for="searchInput" class="sr-only">{{ trans('messages.actions.search') }}</label>
+
+            <div class="input-group">
+                <input type="text" class="form-control" id="searchInput" name="search" value="{{ $search ?? '' }}" placeholder="{{ trans('messages.actions.search') }}">
+
+                <div class="input-group-append">
+                    <button type="submit" class="btn btn-outline-secondary">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </form>
+
     <div class="card shadow mb-4">
         <div class="card-body">
             <div class="table-responsive">
