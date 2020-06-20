6a39e86a3efe4764d0e0a7791481ac519d8e4956
Javdu10 <jav.valentin@gmail.com>
2020-06-20 16:09:00 +0200

    styleci v2

--- app/Games/ProtocolAdapters/Quake3Protocol.php
+++ app/Games/ProtocolAdapters/Quake3Protocol.php
@@ -23,114 +23,116 @@ namespace Azuriom\Games\ProtocolAdapters;
  */
 class Quake3Protocol
 {
-	private $address;
-	private $port;
+
+    private $address;
+    private $port;
     private $rconpassword = false;
     private $fp;
-	private $lastPing = false;
+    private $lastPing = false;
 
-	public function __construct($address, $port, &$success = NULL, &$errno = NULL, &$errstr = NULL)
-	{
-    	$this->address = $address;
-    	$this->port = $port;
+    public function __construct($address, $port, &$success = null, &$errno = null, &$errstr = null)
+    {
+        $this->address = $address;
+        $this->port = $port;
         $this->fp = fsockopen("udp://$address", $port, $errno, $errstr, 5);
         if (! $this->fp) {
-        	$success = false;
+            $success = false;
         } else {
-        	$success = true;
+            $success = true;
         }
     }
-	public function setRconassword($pw)
-	{
+
+    public function setRconassword($pw)
+    {
         $this->rconpassword = $pw;
-	}
+    }
 
-	public function rcon($str)
-	{
-    	if (! $this->rconpassword) {
-    		return false;
-    	}
-    	$this->send("rcon " . $this->rconpassword . " $str");
-		return $this->getResponse();
-	}
+    public function rcon($str)
+    {
+        if (! $this->rconpassword) {
+            return false;
+        }
+        $this->send("rcon " . $this->rconpassword . " $str");
+        return $this->getResponse();
+    }
 
-	private function send($str)
-	{
+    private function send($str)
+    {
         fwrite($this->fp, "\xFF\xFF\xFF\xFF$str\x00");
-	}
+    }
 
-	private function getResponse()
-	{
-    	stream_set_timeout($this->fp, 0, 7e5);
+    private function getResponse()
+    {
+        stream_set_timeout($this->fp, 0, 7e5);
         $s = '';
-	    $start = microtime(true);
+        $start = microtime(true);
         do {
-        	$read = fread($this->fp, 9999);
-			$s .= substr($read, strpos($read, "\n") + 1);
-    		if (!isset($end)) {
-    			$end = microtime(true);
-    		}
-			$info = stream_get_meta_data($this->fp);
-		}
-		while (!$info["timed_out"]);
-		$this->lastPing = round(($end - $start) * 1000);
+            $read = fread($this->fp, 9999);
+            $s .= substr($read, strpos($read, "\n") + 1);
+            if (!isset($end)) {
+                $end = microtime(true);
+            }
+            $info = stream_get_meta_data($this->fp);
+        }
+        while (!$info["timed_out"]);
+        $this->lastPing = round(($end - $start) * 1000);
 
         return $s;
-	}
+    }
 
-	public function quit()
-	{
-    	if (is_resource($this->fp)) {
-			fclose($this->fp);
-			return true;
-    	}
-    	return false;
-	}
+    public function quit()
+    {
+        if (is_resource($this->fp)) {
+            fclose($this->fp);
+            return true;
+        }
+        return false;
+    }
 
-	public function reconnect()
-	{
-    	$this->quit();
-    	$this->__construct($this->address, $this->port);
-	}
+    public function reconnect()
+    {
+        $this->quit();
+        $this->__construct($this->address, $this->port);
+    }
 
-	public function getGameStatus()
-	{
+    public function getGameStatus()
+    {
         $this->send("getstatus");
         $response = $this->getResponse();
         list($dvarslist, $playerlist) = explode("\n", $response, 2);
-		$dvarslist = explode("\\", $dvarslist);
-		$dvars = array();
-		for ($i = 1; $i < count($dvarslist); $i += 2) {
-			$dvars[$dvarslist[$i]] = $dvarslist[$i + 1];
-		}
-		$playerlist = explode("\n", $playerlist);
-		array_pop($playerlist);
-		$players = array();
-		foreach ($playerlist as $value) {
-			list($score, $ping, $name) = explode(" ", $value, 3);
-			$players[] = array(
-				"name" =>substr($name, 1, -1),
-				"score" => $score,
-				"ping" => $ping
-			);
-		}
-		return array($dvars, $players);
-	}
+        $dvarslist = explode("\\", $dvarslist);
+        $dvars = array();
+        for ($i = 1; $i < count($dvarslist); $i += 2) {
+            $dvars[$dvarslist[$i]] = $dvarslist[$i + 1];
+        }
+        $playerlist = explode("\n", $playerlist);
+        array_pop($playerlist);
+        $players = array();
+        foreach ($playerlist as $value) {
+            list($score, $ping, $name) = explode(" ", $value, 3);
+            $players[] = array(
+                "name" =>substr($name, 1, -1),
+                "score" => $score,
+                "ping" => $ping
+            );
+        }
+        return array($dvars, $players);
+    }
 
-	public function getGameInfo()
-	{
+    public function getGameInfo()
+    {
         $this->send("getinfo");
         $response = $this->getResponse();
         $dvarslist = explode("\\", $response);
-		$dvars = array();
-		for ($i = 1; $i < count($dvarslist); $i += 2) {
-			$dvars[$dvarslist[$i]] = $dvarslist[$i + 1];
-		}
-		return $dvars;
-	}
+        $dvars = array();
+        for ($i = 1; $i < count($dvarslist); $i += 2) {
+            $dvars[$dvarslist[$i]] = $dvarslist[$i + 1];
+        }
+        return $dvars;
+    }
 
-	public function getLastPing()
-	{
+    public function getLastPing()
+    {
         return $this->lastPing;
     }
 }
