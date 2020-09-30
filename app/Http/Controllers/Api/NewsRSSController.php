74b1b3201c4829a0de6b29e8751d92fc32167441
Ulysse Ballesteros <ulysseballesteros@gmail.com>
2020-09-30 20:45:19 +0200

    Update NewsRSSController.php

--- app/Http/Controllers/Api/NewsRSSController.php
+++ app/Http/Controllers/Api/NewsRSSController.php
@@ -22,41 +22,41 @@ class NewsRSSController extends Controller
 
         $xml = '<?xml version="1.0" encoding="utf-8"?>';
 
-        $xml = $xml.'<rss version="2.0">';
+        $xml .= '<rss version="2.0">';
 
-        $xml = $xml.'<channel>';
+        $xml .= '<channel>';
 
-        $xml = $xml.'<title>'.site_name().'</title>';
-        $xml = $xml.'<link>'.route('home').'</link>';
-        $xml = $xml.'<language>'.setting('locale').'</language>';
-        $xml = $xml.'<description>'.setting('description', '').'</description>';
-        $xml = $xml.'<webMaster>'.setting('mail.from.address').'</webMaster>';
+        $xml .= '<title>{site_name()}</title>';
+        $xml .= '<link>'.route('home')}</link>';
+        $xml .= '<language>'.setting('locale')}</language>';
+        $xml .= '<description>'.setting('description', '')}</description>';
+        $xml .= '<webMaster>'.setting('mail.from.address')}</webMaster>';
 
-        $xml = $xml.'<image>';
-        $xml = $xml.' <title>'.site_name().'</title>';
-        $xml = $xml.'<url>'.image_url(setting('logo')).'</url>';
-        $xml = $xml.'<link>'.route('home').'</link>';
-        $xml = $xml.'</image>';
+        $xml .= '<image>';
+        $xml .= ' <title>{site_name()}</title>';
+        $xml .= .'<url>{image_url(setting('logo'))}</url>';
+        $xml .= '<link>{route('home')}</link>';
+        $xml .= '</image>';
 
         foreach ($posts as $post) {
-            $xml = $xml.'<item>';
-            $xml = $xml.'<title>'.$post->title.'</title>';
+            $xml .= '<item>';
+            $xml .= '<title>{$post->title}</title>';
             if ($post->hasImage()) {
-                $xml = $xml.'<image>'.$post->imageUrl().'</image>';
+                $xml .= '<image>{$post->imageUrl()}</image>';
             } else {
-                $xml = $xml.'<image></image>';
+                $xml .= '<image></image>';
             }
 
-            $xml = $xml.'<link>'.route('posts.show', $post).'</link>';
-            $xml = $xml.'<content>'.$post->content.'</content>';
-            $xml = $xml.'<pubDate>'.$post->published_at.'</pubDate>';
-            $xml = $xml.'<author>'.$post->author->name.'</author>';
-            $xml = $xml.'<comments>'.count($post->comments).'</comments>';
-            $xml = $xml.'</item>';
+            $xml .= '<link>{route('posts.show', $post)}</link>';
+            $xml .= '<content>{$post->content}</content>';
+            $xml .= '<pubDate>{$post->published_at}</pubDate>';
+            $xml .= '<author>{$post->author->name}</author>';
+            $xml .= '<comments>{count($post->comments)}</comments>';
+            $xml .= '</item>';
         }
 
-        $xml = $xml.'</channel>';
-        $xml = $xml.'</rss>';
+        $xml .= '</channel>';
+        $xml .= '</rss>';
 
         return response($xml, 200)
             ->header('Content-Type', 'application/xml');
