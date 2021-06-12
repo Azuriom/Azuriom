ac1129994ecb74cbea3aaaac4d6a604659bb4c8e
Jav <jav.valentin@gmail.com>
2021-06-12 15:44:45 +0200

    Update Azuriom.php

--- app/Azuriom.php
+++ app/Azuriom.php
@@ -9,7 +9,7 @@ class Azuriom
      *
      * @var string
      */
-    private const VERSION = '0.3.8';
+    private const VERSION = '0.3.7';
 
     /**
      * Get the current version of Azuriom CMS.
