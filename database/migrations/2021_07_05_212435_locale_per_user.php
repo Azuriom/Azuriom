9f3b05944b3a1237fe1ace49da6a5a41a5cfb439
Javdu10 <jav.valentin@gmail.com>
2021-07-06 22:41:17 +0200

    missing import

--- database/migrations/2021_07_05_212435_locale_per_user.php
+++ database/migrations/2021_07_05_212435_locale_per_user.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\App;
 
 class LocalePerUser extends Migration
 {
