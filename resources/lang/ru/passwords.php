587eeedf63983cb7a222a57fe19b0031fd12a97b
FuturAura <markpikaro@yandex.com>
2022-04-02 15:26:50 +0200

    Add russian translate

---
+++ resources/lang/ru/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Ваш пароль был сброшен!',
+    'sent' => 'Мы отправили вам по электронной почте ссылку для сброса пароля!',
+    'throttled' => 'Пожалуйста, подождите, прежде чем повторить попытку.',
+    'token' => 'Этот токен сброса пароля недействителен.',
+    'user' => "Мы не можем найти пользователя с этим адресом электронной почты.",
+
+];
