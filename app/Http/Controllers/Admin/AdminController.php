29d7d0981666542b36cf35530c326b6368e10412
Jav <jav.valentin@gmail.com>
2022-05-27 10:59:42 +0200

    Count active users through a query and not with ->each()

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -11,6 +11,7 @@ use Azuriom\Models\User;
 use Azuriom\Support\Charts;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
@@ -57,37 +58,26 @@ class AdminController extends Controller
 
     protected function getActiveUsers()
     {
-        $days = [1, 7, 31];
+        $condition = match (config('database.default')) {
+            'sqlsrv' => 'DATEDIFF(day, [last_login_at], GETDATE())',
+            'pgsql' => 'NOW()::date - last_login_at::date',
+            'sqlite' => 'julianday() - julianday(last_login_at)',
+            default => 'DATEDIFF(NOW(), last_login_at)', // mysql
+        };
+        /*
+        * query structure :
+        * 1 : last_login_at whitin a day
+        * 2 : last_login_at whitin a week but not the first day of the week
+        * 3 : last_login_at whitin a month but not the first week of the month
+        * 4 : last_login_at more than a month
+        */
+        $query = <<<LINE
+        COUNT(CASE WHEN {$condition} < 2 then 1 ELSE NULL END) as "1",
+        COUNT(CASE WHEN {$condition} > 1 AND {$condition} < 8 then 1 ELSE NULL END) as "7",
+        COUNT(CASE WHEN {$condition} > 7 AND {$condition} < 32 then 1 ELSE NULL END) as "31",
+        COUNT(CASE WHEN  {$condition} > 31 then 1 ELSE NULL END) as "+1 month"
+        LINE;
 
-        $users = collect([
-            1 => 0,
-            7 => 0,
-            31 => 0,
-        ]);
-
-        User::where('last_login_at', '>=', now()->subMonth())
-            ->without('role')
-            ->get(['id', 'last_login_at'])
-            ->each(function ($user) use ($days, $users) {
-                $diff = $user->last_login_at->diffInDays();
-
-                foreach ($days as $time) {
-                    if ($diff <= $time) {
-                        $users->put($time, $users->get($time, 0) + 1);
-                        break;
-                    }
-                }
-            });
-
-        $users = $users->mapWithKeys(function ($value, $key) {
-            $time = now()->subDays($key)->longAbsoluteDiffForHumans();
-
-            return [$time => $value];
-        });
-
-        $oldUsers = User::whereDate('last_login_at', '<', now()->subMonth())->count();
-        $users->put('+ '.now()->subMonth()->longAbsoluteDiffForHumans(), $oldUsers);
-
-        return $users;
+        return (array) DB::table('users')->select(DB::raw($query))->whereNotNull('last_login_at')->first();
     }
 }
