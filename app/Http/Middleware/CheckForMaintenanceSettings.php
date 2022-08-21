2fadcf7657392b5a77595a6d1afb7d83443e3603
MrMicky <git@mrmicky.fr>
2022-08-21 10:32:50 +0200

    Prevent issue when maintenance paths are empty

--- app/Http/Middleware/CheckForMaintenanceSettings.php
+++ app/Http/Middleware/CheckForMaintenanceSettings.php
@@ -56,7 +56,7 @@ class CheckForMaintenanceSettings
             return Str::endsWith($path, '/*')
                 ? Str::replaceLast('/*', '*', $path)
                 : $path;
-        }, setting('maintenance-paths'));
+        }, setting('maintenance-paths') ?? []);
 
         if (! empty($blockedPaths) && ! $request->is($blockedPaths)) {
             return $next($request);
