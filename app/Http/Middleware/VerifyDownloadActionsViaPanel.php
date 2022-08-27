25b0f9067d6ee88083d4c3e664915672a2a28245
magrigry <>
2022-08-27 18:23:02 +0200

    fix: style CI

--- app/Http/Middleware/VerifyDownloadActionsViaPanel.php
+++ app/Http/Middleware/VerifyDownloadActionsViaPanel.php
@@ -7,10 +7,10 @@ use Illuminate\Http\Request;
 
 class VerifyDownloadActionsViaPanel
 {
-
     public function handle(Request $request, Closure $next)
     {
         abort_if(! env('ALLOW_PANEL_DOWNLOAD_ACTIONS', true), 405, 'This function is disabled.');
+
         return $next($request);
     }
 }
