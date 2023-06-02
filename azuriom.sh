e6b4e4dad9841c505330c30f12e46477147de5ce
Charles <charles.boitel@next4.io>
2023-06-03 00:01:59 +0200

    Add new docker-compose and Dockerfile for arm64v8

--- azuriom.sh
+++ azuriom.sh
@@ -1,105 +1,124 @@
 #!/bin/sh
 
+# Check if .env file exists
+if [ ! -f .env ]; then
+    echo "The .env file does not exist at the root of the project. You must copy .env.example to .env and modify the database values for the script to execute correctly."
+    exit 1
+fi
+
+ARCH=$(uname -m)
+
+# shellcheck disable=SC2039
+if [[ "$ARCH" == "arm64" ]]; then
+    NODE_IMAGE="arm64v8/node:18"
+    COMPOSER_IMAGE="arm64v8/composer"
+    DOCKER_COMPOSE_FILE="docker-compose-arm.yml"
+else
+    NODE_IMAGE="node:18"
+    COMPOSER_IMAGE="composer"
+    DOCKER_COMPOSE_FILE="docker-compose.yml"
+fi
+
 SCRIPT=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT")
 
 cd "${SCRIPT_DIR}" || exit
 
 case $1 in
-
   "build")
     bash "$0" composer-install
     bash "$0" npm-install
     bash "$0" npm-run-prod
     bash "$0" laravel-symlink
-    docker compose build
+    docker-compose -f $DOCKER_COMPOSE_FILE build
     exit
     ;;
 
   "start")
-    docker compose up -d
+    docker-compose -f $DOCKER_COMPOSE_FILE up -d
     exit
     ;;
 
   "stop")
-    docker compose stop
+    docker-compose -f $DOCKER_COMPOSE_FILE stop
     exit
     ;;
 
   "docker-compose-build")
-    docker compose build
+    docker-compose -f $DOCKER_COMPOSE_FILE build
     exit
     ;;
 
   "npm-install")
-    docker run -it --rm -v "${SCRIPT_DIR}":/usr/src/app -w /usr/src/app  node:18 npm ci
+    docker run -it --rm -v "${SCRIPT_DIR}":/usr/src/app -w /usr/src/app $NODE_IMAGE npm ci
     exit
     ;;
 
   "npm-run-prod")
-    docker run -it --rm -v "${SCRIPT_DIR}":/usr/src/app -w /usr/src/app  node:18 npm run prod
+    docker run -it --rm -v "${SCRIPT_DIR}":/usr/src/app -w /usr/src/app $NODE_IMAGE npm run prod
     exit
     ;;
 
   "composer-install")
-    docker run --rm --interactive --tty --volume "${SCRIPT_DIR}":/app composer install
+    docker run --rm --interactive --tty --volume "${SCRIPT_DIR}":/app $COMPOSER_IMAGE install
     exit
     ;;
 
   "laravel-generate-key")
-    docker compose exec php-fpm php artisan key:generate
+    docker-compose -f $DOCKER_COMPOSE_FILE exec php-fpm php artisan key:generate
     exit
     ;;
 
   "laravel-init-db")
-    docker compose exec php-fpm php artisan migrate --seed
+    docker-compose -f $DOCKER_COMPOSE_FILE exec php-fpm php artisan migrate --seed
     exit
     ;;
 
   "laravel-migrate")
-    docker compose exec php-fpm php artisan migrate
+    docker-compose -f $DOCKER_COMPOSE_FILE exec php-fpm php artisan migrate
     exit
     ;;
 
   "laravel-symlink")
-    docker compose exec php-fpm php artisan storage:link
+    docker-compose -f $DOCKER_COMPOSE_FILE exec php-fpm php artisan storage:link
     exit
     ;;
 
   "laravel-create-admin")
-    docker compose exec php-fpm php artisan user:create --admin
+    docker-compose -f $DOCKER_COMPOSE_FILE exec php-fpm php artisan user:create --admin
     exit
     ;;
 
   "laravel-clear-cache")
-    docker compose exec php-fpm php artisan cache:clear
+    docker-compose -f $DOCKER_COMPOSE_FILE exec php-fpm php artisan cache:clear
     exit
     ;;
 
   "artisan")
     shift
-    docker compose exec php-fpm php artisan "$*"
+    docker-compose -f $DOCKER_COMPOSE_FILE exec php-fpm php artisan "$*"
     exit
     ;;
 
 esac
 
-   # Display Help
-   echo "Manage Azuriom using docker and docker-compose. See https://github.com/Azuriom/Azuriom/blob/master/docker/INSTALL.md"
-   echo
-   echo "Usage: ./azuriom.sh COMMAND [arguments]"
-   echo
-   echo "Commands:"
-   echo "   build                      Build the whole application."
-   echo "   start                      Start containers (require the whole application to be built)"
-   echo "   docker-compose-build       Build containers"
-   echo "   npm-install                Install npm dependencies"
-   echo "   npm-run-prod               Compile assets (require npm dependencies to be installed)"
-   echo "   composer-install           Install composer dependencies"
-   echo "   laravel-generate-key       Generate laravel APP_KEY in .env file (used by laravel for data encryption)"
-   echo "   laravel-init-db            Initiate database"
-   echo "   laravel-migrate            Run migration (mostly used when updating to insert database changes)"
-   echo "   laravel-symlink            Create a symlink (create a symlink public/storage -> storage/app/public)"
-   echo "   laravel-create-admin       Create an admin user through CLI"
-   echo "   laravel-clear-cache        Clear laravel cache"
-   echo "   artisan <commands>         Run any artisan command (e.g. ./azuriom artisan cache:clear)"
+# Display Help
+echo "Manage Azuriom using docker and docker-compose. See https://github.com/Azuriom/Azuriom/blob/master/docker/INSTALL.md"
+echo
+echo "Usage: ./azuriom.sh COMMAND [arguments]"
+echo
+echo "Commands:"
+echo "   build                      Build the whole application."
+echo "   start                      Start containers (require the whole application to be built)"
+echo "   stop                       Stop containers"
+echo "   docker-compose-build       Build containers"
+echo "   npm-install                Install npm dependencies"
+echo "   npm-run-prod               Compile assets (require npm dependencies to be installed)"
+echo "   composer-install           Install composer dependencies"
+echo "   laravel-generate-key       Generate laravel APP_KEY in .env file (used by laravel for data encryption)"
+echo "   laravel-init-db            Initiate database"
+echo "   laravel-migrate            Run migration (mostly used when updating to insert database changes)"
+echo "   laravel-symlink            Create a symlink (create a symlink public/storage -> storage/app/public)"
+echo "   laravel-create-admin       Create an admin user through CLI"
+echo "   laravel-clear-cache        Clear laravel cache"
+echo "   artisan <commands>         Run any artisan command (e.g. ./azuriom artisan cache:clear)"
