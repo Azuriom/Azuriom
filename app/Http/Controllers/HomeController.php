6ef55aac4b25363dc2391720a899037914abda94
MrMicky <git@mrmicky.fr>
2023-10-14 19:01:55 +0200

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,9 +17,10 @@ class HomeController extends Controller
             ->latest('published_at')
             ->take(5)
             ->get();
+        $homeMessage = setting('home_message');
 
         return view('home', [
-            'message' => empty(setting('home_message')) ? null : new HtmlString(setting('home_message')),
+            'message' => $homeMessage ? new HtmlString($homeMessage) : null,
             'posts' => $posts,
         ]);
     }
