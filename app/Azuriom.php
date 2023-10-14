7bae14a04b98d34682e4af9dc5dc02386efb67b4
MrMicky <git@mrmicky.fr>
2023-10-14 16:31:22 +0200

    Update version

--- app/Azuriom.php
+++ app/Azuriom.php
@@ -11,7 +11,7 @@ class Azuriom
      *
      * @var string
      */
-    private const VERSION = '1.1.4';
+    private const VERSION = '1.1.5';
 
     /**
      * Get the current version of Azuriom CMS.
