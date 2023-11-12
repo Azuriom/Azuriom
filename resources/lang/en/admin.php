6e4ffd2a128857abcb9c733d1d6648de82c46a0c
Gru <ruslru376@gmail.com>
2023-11-12 13:53:10 +1000

    typo error

--- resources/lang/en/admin.php
+++ resources/lang/en/admin.php
@@ -270,7 +270,7 @@ return [
         'rcon_password' => 'Rcon Password',
         'rcon_port' => 'Rcon Port',
         'query_port' => 'Source Query Port',
-        'unturned_info' => 'You need to use srcds RCON type in OpenMod. RocketMod RCON is not compatitable!',
+        'unturned_info' => 'You need to use srcds RCON type in OpenMod. RocketMod RCON is not compatible!',
 
         'azlink' => [
             'port' => 'AzLink Port',
