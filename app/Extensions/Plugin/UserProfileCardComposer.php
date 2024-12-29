ddd265905e27d0c845e32e5a12f304ea7d91c454
Alexander Shakhmukhametov (RU-WM) <diablus2012@gmail.com>
2024-12-29 18:04:55 +0300

    Update UserProfileCardComposer.php
    data array to profile custom card view

--- app/Extensions/Plugin/UserProfileCardComposer.php
+++ app/Extensions/Plugin/UserProfileCardComposer.php
@@ -9,6 +9,7 @@ abstract class UserProfileCardComposer extends AdminDashboardCardComposer
      * Each card should contain:
      * - 'name' : The title of the card
      * - 'view' : The view (Ex: shop::giftcards.index).
+     * - 'data' : The data array for view (Ex: compact('user'))
      *
      * @return array{name: string, view: string}[]
      */
