5ddd95f312dfe92085c2aee61de4033e5a5b45a6
MrMicky <git@mrmicky.fr>
2024-12-29 17:42:26 +0100

    Formatting

--- app/Extensions/Plugin/UserProfileCardComposer.php
+++ app/Extensions/Plugin/UserProfileCardComposer.php
@@ -8,10 +8,10 @@ abstract class UserProfileCardComposer extends AdminDashboardCardComposer
      * Get the cards to add to the user profile.
      * Each card should contain:
      * - 'name' : The title of the card
-     * - 'view' : The view (Ex: shop::giftcards.index).
-     * - 'data' : The data array for view (Ex: compact('user'))
+     * - 'view' : The view (Ex: shop::giftcards.index)
+     * - 'data' : The data array for view (Ex: ['info' => $info]).
      *
-     * @return array{name: string, view: string, data: mixed}[]
+     * @return array{name: string, view: string, data?: array}[]
      */
     abstract public function getCards();
 }
