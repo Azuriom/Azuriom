b59c0a209993c6123831929c4aaccab02fe97f88
Alexander Shakhmukhametov (RU-WM) <diablus2012@gmail.com>
2024-12-29 19:49:34 +0300

    Support custom data for profile cards (#480)

--- app/Extensions/Plugin/UserProfileCardComposer.php
+++ app/Extensions/Plugin/UserProfileCardComposer.php
@@ -8,9 +8,10 @@ abstract class UserProfileCardComposer extends AdminDashboardCardComposer
      * Get the cards to add to the user profile.
      * Each card should contain:
      * - 'name' : The title of the card
-     * - 'view' : The view (Ex: shop::giftcards.index).
+     * - 'view' : The view (Ex: shop::giftcards.index)
+     * - 'data' : (Optional) The data array for the view (Ex: ['info' => $info]).
      *
-     * @return array{name: string, view: string}[]
+     * @return array{name: string, view: string, data?: array}[]
      */
     abstract public function getCards();
 }
