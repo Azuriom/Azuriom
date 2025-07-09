40ce9a0d8916dcbad8e6b1ceecbcd6d30a980203
Clémentbo <clementbo50@gmail.com>
2025-07-09 12:54:59 +0200

    Add post search by title or description in PostController
    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,21 +4,40 @@ namespace Azuriom\Http\Controllers;
 
 use Azuriom\Models\Post;
 use Illuminate\Contracts\Database\Query\Builder;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of published resources.
+     *
+     * If no search term is provided, all published resources are returned. If a search term is
+     * provided via the 'search' query parameter, resources are filtered by title or description.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\View\View
      */
-    public function index()
+    public function index(Request $request)
     {
-        $posts = Post::published()
-            ->with('author')
-            ->orderByDesc('is_pinned')
-            ->latest('published_at')
-            ->get();
+        $search = trim($request->input('search', ''));
 
-        return view('posts.index', ['posts' => $posts]);
+        $query = Post::published()->with('author');
+
+        $searchTerm = mb_strtolower($search, 'UTF-8');
+        $query->where(function ($q) use ($searchTerm) {
+            $q->where('title', 'LIKE', "%{$searchTerm}%")
+              ->orWhere('description', 'LIKE', "%{$searchTerm}%");
+        });
+
+        $posts = $query->orderByDesc('is_pinned')
+                       ->latest('published_at')
+                       ->paginate()
+                       ->withQueryString();
+
+        return view('posts.index', [
+            'posts' => $posts,
+            'search' => $search,
+        ]);
     }
 
     /**
